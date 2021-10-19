221c2322776d8c1c29ac169e0b942cae261b6960
Mario Martinez <mariomtz091@gmail.com>
2021-10-18 22:03:45 -0300

    Proyecto Mercado Liebre

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PUERTO = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+app.use(express.static("public"));
+
+app.listen(PUERTO, () => {
+  console.log(`Server ${PUERTO} está UP`);
+});
